92821e24cd8c516f6cc8061bf8122897450d0b48
Nodirbek <berdimurodovnodir89@gmail.com>
2025-12-18 15:23:19 +0500

    bajarildi

---
+++ task16.py
@@ -0,0 +1,45 @@
+ball = int(input(f' Ball 1:'))
+max_ball = ball
+
+ball = int(input(f' Ball 2:'))
+max_ball = ball
+if ball > max_ball :
+    max_ball = ball
+
+ball = int(input(f' Ball 3:'))
+max_ball = ball
+
+max_ball = ball
+if ball > max_ball :
+    max_ball = ball
+    
+ball = int(input(f' Ball 4:'))
+max_ball = ball
+
+max_ball = ball
+if ball > max_ball :
+    max_ball = ball
+    
+ball = int(input(f' Ball 5:'))
+max_ball = ball
+
+max_ball = ball
+if ball > max_ball :
+    max_ball = ball
+    
+ball = int(input(f' Ball 6:'))
+max_ball = ball
+
+max_ball = ball
+if ball > max_ball :
+    max_ball = ball
+    
+ball = int(input(f' Ball 7:'))
+max_ball = ball
+
+max_ball = ball
+if ball > max_ball :
+    max_ball = ball
+    
+print(max_ball)
+
