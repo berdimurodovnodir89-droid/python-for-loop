6d15bedc9c47baeed8602e10b8d12dd31ea42d47
Nodirbek <berdimurodovnodir89@gmail.com>
2025-12-19 17:24:38 +0500

    bajarildi

--- task19.py
+++ task19.py
@@ -1,10 +1,17 @@
 ball = int(input(f' Ball 1:'))
-max_ball = ball
- 
+min_ball = ball
+
 for i in range(2,6) :
     ball = int(input(f' Ball 2:'))
-    if ball < max_ball :
-     max_ball = ball
+    if ball < min_ball :
+     min_ball = ball
 
+print(min_ball)
+
+for i in range(2,6) :
+    ball = int(input(f' Ball 2:'))
+    if ball > max_ball :
+     max_ball = ball
 print(max_ball)
-  
+
+print(max_ball + min_ball)
---
+++ task20.py
@@ -0,0 +1,13 @@
+ball = int(input(f' Ball 1:'))
+max_ball = ball
+ 
+for i in range(2,6) :
+    ball = ball = int(input(f' Ball 2:'))
+
+ball = int(input(f' Ball 2:')) 
+max_ball = ball
+if ball > max_ball :
+        max_ball = ball
+
+print(max_ball)
+  
